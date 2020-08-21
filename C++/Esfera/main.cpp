28523e4700db54cbaa58cccd82654b2e0ea1b124
Agustín Argüello <66805428+Dinamo0809@users.noreply.github.com>
2020-08-21 12:21:11 -0300

    Add files via upload
    Programa hecho con C++ en el que se calcula el volumen, circunferencia y área de una esfera ingresada.

---
+++ C++/Esfera/main.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <iomanip>
+# include <cmath>
+
+using namespace std;
+
+int main(void) {
+    //Se declaran las variables
+    const float PI = 3.1416;
+    float radio;
+    cout << "Por favor, introduce el valor del radio de un esfera: ";
+    cin >> radio;
+    cout << "\n Longitud de la circunferecnia: " << fixed << setprecision(2) << 2 * PI *  radio;
+    cout << "\n Area del circulo: " << PI * pow(radio,2);
+    cout << "\n Volumen de la esfera: " << (4.0/3) * PI * pow(radio, 3) << endl;
+    system("pause");
+}
